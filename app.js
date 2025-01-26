30499ae2b8218f6d3a083c2b87f725224af58fe9
MuhamedSuhailSJ <s.j.muhamedsuhail@outlook.in>
2025-01-26 11:19:23 +0530

    phase2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const db = require("./config")
+const db = require("./config");
 
 const users = require("./module/user");
 const customers = require("./module/customer");
@@ -25,14 +25,20 @@ app.listen(3000);
 
 const authentication = async (request, response, next) => {
   const token = request.headers.cookie;
-  response.send(token);
-  jwt.verify(token, "SECRETTOKEN", async (error, payload) => {
+  if (token === undefined) {
+    response.send("Please Login");
+    return;
+  }
+  console.log(token.split("=")[1]);
+  jwt.verify(token.split("=")[1], "SECRETTOKEN", async (error, payload) => {
     if (error) {
+      console.log("Error in Logging");
       console.log(error);
     } else {
+      console.log("Loged In");
+      console.log(payload);
       request.payload = payload;
-      request.id = id;
-      request.body = { name, email, phone, company };
+      next();
     }
   });
 };
@@ -65,9 +71,9 @@ app.post("/login", async (request, response) => {
     response.status(400);
   } else {
     const isPasswordMatched = await bcrypt.compare(password, userfind.password);
-      if (isPasswordMatched) {
-          const jwtToken = jwt.sign(userfind, "SECRETTOKEN");
-          response.cookie("JwtToken", {jwtToken});
+    if (isPasswordMatched) {
+      const jwtToken = jwt.sign(userfind, "SECRETTOKEN");
+      response.cookie("JwtToken", jwtToken);
       response.send({ jwtToken });
     } else {
       response.send("Invalid Password");
@@ -78,32 +84,45 @@ app.post("/login", async (request, response) => {
 
 app.get("/customers", authentication, async (request, response) => {
   const { payload } = request;
-  const { user_id } = payload;
-  const getAllCustomers = await customers.findAll({ where: { user_id } });
-  response.send("All Customers Shown");
-  response.status(200);
+  console.log(payload);
+  const { id } = payload;
+  console.log(id);
+  const getAllCustomers = await customers.findAll({ where: { userId: id } });
+  response.send("Customer Got" + JSON.stringify(getAllCustomers));
+  console.log(JSON.stringify(getAllCustomers));
 });
 
 app.get("/customers/:id", authentication, async (request, response) => {
   const { id } = request.params;
-  const { payload } = request;
-  const getAllCustomers = await customers.findAll({ where: { id } });
-  response.send(`All ${id} Customer Shown`);
-  response.status(200);
+  const userId = request.payload.id;
+  const getIdCustomers = await customers.findOne({ where: { id, userId } });
+  if (JSON.parse(JSON.stringify(getIdCustomers)).id === id) {
+    response.send(`Customer ${id} Got`);
+    response.status(200);
+  } else {
+    response.send("Customer Not Exist, Please Check It");
+    response.status(401);
+  }
 });
 
 app.post("/customers", authentication, async (request, response) => {
   const { name, email, phone, company } = request.body;
-  const { user_id } = payload;
-  const createCustomer = await customers.create({
-    name,
-    email,
-    phone,
-    company,
-    user_id,
-  });
-  response.send("Customer Created Sucessfully");
-  response.status(200);
+  const { id } = request.payload;
+  const checkExistingCustomer = await customers.findOne({ where: { email } });
+  if (JSON.stringify(checkExistingCustomer).email === email) {
+    const createCustomer = await customers.create({
+      name,
+      email,
+      phone,
+      company,
+      userId: id,
+    });
+    response.send("Customer Created Sucessfully");
+    response.status(200);
+  } else {
+    response.send("Customer Already Existed");
+    response.status(401);
+  }
 });
 
 app.put("/customers/:id", authentication, async (request, response) => {
@@ -117,7 +136,7 @@ app.put("/customers/:id", authentication, async (request, response) => {
   response.status(400);
 });
 
-app.put("/customers/:id", authentication, async (request, response) => {
+app.delete("/customers/:id", authentication, async (request, response) => {
   const { id } = request.params;
   const deleteCustomer = await customers.destroy({ where: { id } });
   response.send("Deleted Successfully");
